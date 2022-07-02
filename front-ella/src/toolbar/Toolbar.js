8a3422629b65914666238ce385dc966998a9c428
Lina Diringer <lina.diringer@epita.fr>
2022-07-02 15:23:26 +0200

    keyboard + toolbar

---
+++ front-ella/src/toolbar/Toolbar.js
@@ -0,0 +1,49 @@
+import "./toolbar-styles.css";
+import React, { useState } from "react";
+import logo from "./totally_spies.jpg";
+import buttonimg from "./button-bar.png";
+
+function PrefMenu() {
+  return (
+    <div class="menu">
+      <button class="file-button">Preference file</button>
+      <button class="run-shortcut">Run shortcut</button>
+      <button class="search-shortcut">Search shortcut</button>
+      <button class="ts-theme-shortcut">Totally Spies theme shortcut</button>
+    </div>
+  );
+}
+
+export default function ToolBar() {
+  // switch button
+  const [isToggled, setIsToggled] = useState(false);
+  const onToggle = () => setIsToggled(!isToggled);
+
+  // preference menu
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const buttonClick = () => setIsMenuOpen(!isMenuOpen);
+
+  return (
+    <div class="Layout">
+      <div class="ToolBar">
+        <img class="logo" src={logo} />
+        <div class="ide-name">ELLA IDE</div>
+        <div class="switch-element">
+          <div class="toggle-switch">
+            <label className="toggle-switch">
+              <input type="checkbox" checked={isToggled} onChange={onToggle} />
+              <span className="switch" />
+            </label>
+          </div>
+          <div class="button-text">Keyboard</div>
+        </div>
+        <div>
+          <button class="preference-button" type="button" onClick={buttonClick}>
+            <img src={buttonimg} width="40" />
+          </button>
+        </div>
+      </div>
+      {isMenuOpen && <PrefMenu />}
+    </div>
+  );
+}
