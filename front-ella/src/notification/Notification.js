dd37ff246b207ecb1bd64995436d35976aa00eb1
Lina Diringer <lina.diringer@epita.fr>
2022-07-03 17:08:52 +0200

    add notification layout

---
+++ front-ella/src/notification/Notification.js
@@ -0,0 +1,33 @@
+import "./notification-styles.css";
+import checkImg from "./check_icon.png";
+import errorImg from "./error-icon.png";
+
+export default function Notification() {
+  var success = false;
+  return (
+    <div className="Notification">{success ? <Success /> : <Error />}</div>
+  );
+}
+
+function Success() {
+  return (
+    <div class="layout">
+      <div class="header">
+        <img class="check" src={checkImg} width="10%" />
+        <div class="title">Build successful</div>
+      </div>
+    </div>
+  );
+}
+
+function Error() {
+  return (
+    <div class="layout">
+      <div class="header">
+        <img class="error-img" src={errorImg} width="10%" />
+        <div class="title">Error</div>
+      </div>
+      <div class="message">**error message**</div>
+    </div>
+  );
+}
