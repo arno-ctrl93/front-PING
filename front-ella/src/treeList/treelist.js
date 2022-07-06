913d6f74c878d353632dc801078970d21df9c19d
arno-ctrl93 <ruty.notter@gmail.com>
2022-07-06 03:00:10 +0200

    change treefile

--- front-ella/src/treeList/treelist.js
+++ front-ella/src/treeList/treelist.js
@@ -1,101 +1,75 @@
-import React from "react";
-import { useEffect } from "react";
-import "./treelist.css";
-import axios from 'axios';
+import * as React from 'react';
+import TreeView from '@mui/lab/TreeView';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import TreeItem from '@mui/lab/TreeItem';
 
-import { useTextContext } from '../contexts/TextContext'
-
-const App = () => {
-  let root;
-
-  const { setIsText } = useTextContext();
-  
-  let arbo = "";
-  var arboDiv = document.getElementById("Arbolescence");
-  if (!arboDiv) {
-    console.log("arboDiv not found");
-  }
-
-  function loadFile(path) {
-    console.log("loadFile: "+path);
-    let t = "";
-    axios({
-      method: 'post',
-      url: 'http://localhost:9000/contentFile',
-      data: {
-        path : path,
-      }
-    }).then(function (response) {
-      t = response.data;
-      console.log("file")
-      console.log(response.data);
-    });
-    return t;
-  }
-  function createArbolescence(root, indexSpace, folderToAttach) {
-    console.log("recursive");
-    for (let children of root.children) {
-      if (children.isFolder) 
+const data = {
+  "id": "root",
+  "name": "root",
+  "children": [
       {
-        // is folder
-        var folderUl = document.createElement("li");
-        var checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        folderUl.appendChild(checkbox);
-        folderUl.innerHTML = children["name"];
-        folderUl.style.listStyle = "square";
-        createArbolescence(children, indexSpace + 1, folderUl);
-        folderToAttach.appendChild(folderUl);
-      }
-      else
+          "id": "1",
+          "name": ".git",
+          "children": []
+      },
       {
-        // is file
-        var fileIl = document.createElement("li");
-        fileIl.innerHTML = children["name"];
-        fileIl.onclick = function() {
-          console.log("click");
-          let text = loadFile(children["path"]);
-          setIsText(text);
-        }
-        folderToAttach.appendChild(fileIl);
+          "id": "2",
+          "name": "file1",
+          "children": []
+      },
+      {
+          "id": "3",
+          "name": "file2",
+          "children": []
+      },
+      {
+          "id": "4",
+          "name": "file3",
+          "children": []
+      },
+      {
+          "id": "5",
+          "name": "folder",
+          "children": []
+      },
+      {
+          "id": "6",
+          "name": "folder4",
+          "children": [
+              {
+                  "id": "7",
+                  "name": "file7",
+                  "children": []
+              }
+          ]
+      },
+      {
+          "id": "8",
+          "name": "pom.xml",
+          "children": []
       }
-    }
-  }
+  ]
+}
 
-  useEffect(() => {
-
-    axios({
-      method: 'post',
-      url: 'http://localhost:9000/init',
-      data: {
-        path : "myproject",
-        name : "cc"
-      }
-    }).then(function (response) {
-      root = response.data;
-      console.log("file")
-      console.log(response.data);
-    }).then(function() {
-
-      let Arbolescence = document.getElementById("Arbolescence");
-      if (Arbolescence && root !== undefined) {
-        var folderUl = document.createElement("li");
-        folderUl.innerHTML = root["root"]["name"];
-        folderUl.style.listStyle = "./folder.jpg";
-        createArbolescence(root["root"], 1, folderUl);
-        Arbolescence.appendChild(folderUl);
-      } else
-        console.log("Root undefined")
-    });
-  });
-  return (
-    <div className="App">
-      <center><h4> Fichiers </h4></center>
-      <div id="Arbolescence">
-      </div>
-    </div>
-    
+export default function RichObjectTreeView() {
+  const renderTree = (nodes) => (
+    <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+      {Array.isArray(nodes.children)
+        ? nodes.children.map((node) => renderTree(node))
+        : null}
+    </TreeItem>
   );
-};
 
-export default App;
+  return (
+    <TreeView
+      aria-label="rich object"
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpanded={['root']}
+      defaultExpandIcon={<ChevronRightIcon />}
+      sx={{ height: 110, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+    >
+      {renderTree(data)}
+    </TreeView>
+  );
+}
